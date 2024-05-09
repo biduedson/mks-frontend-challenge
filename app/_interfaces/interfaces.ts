7e316161a41cb460b712d5c88e635c6a8e08010b
Edson <biduzao.bidu21@gmail.com>
2024-05-09 09:31:33 -0300

    chore: create interfaces products and cart product

---
+++ app/_interfaces/interfaces.ts
@@ -0,0 +1,31 @@
+export interface IProduct{
+    id:number;
+    name:string;
+    brand:string;
+    description:string;
+    photo:string;
+    price:number;
+    createdAt:Date;
+    updatedAt:Date;
+
+}
+
+export interface ICart{
+    products: IProduct[];
+    totalPrice: number;
+    totalQuantity:number;
+    addProductToCArt:({
+        product,
+        quantity,
+        emptyCart
+    }:{
+        product:IProduct;
+        quantity:number;
+        emptyCart?:boolean;
+    }
+) => void;
+decreaseProductQuantity:(productId:string) =>void;
+increaseProductQuantity:(productID:string) =>void;
+clearCart:() => void;
+
+}
