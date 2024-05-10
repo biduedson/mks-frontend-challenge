289021603f6993fb5b87b9fe5de23e3145e561af
Edson <biduzao.bidu21@gmail.com>
2024-05-10 12:20:12 -0300

    feadt : add header component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className=" fixed bottom-0 w-full bg-primary-light h-[34px] font-normal  text-[12px] flex items-center justify-center leading-[14.63px]">
+      MKS Sistemas © Todos os direitos reservados
+    </footer>
+  );
+};
+
+export default Footer;
