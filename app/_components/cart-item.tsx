a3ca71b93914fc1fef6a6f5a5a9bc18c7773719e
Edson <biduzao.bidu21@gmail.com>
2024-05-10 22:51:07 -0300

    feat: add  logical of open and close sidebar

--- app/_components/cart-item.tsx
+++ app/_components/cart-item.tsx
@@ -1,16 +1,21 @@
+"use client";
 import Image from "next/image";
 import { ICartProduct } from "../_interfaces/interfaces";
 import { Button } from "./ui/button";
 import { formatCurrency } from "../_helpers/price";
+import { useContext } from "react";
+import { CartContext } from "../_context/data-context";
 
 interface ICartItemProps {
   cartProduct: ICartProduct;
 }
-
 const CartItem = ({ cartProduct }: ICartItemProps) => {
+  const { decreaseProductQuantity, increaseProductQuantity } =
+    useContext(CartContext);
+
   return (
     <div className="flex items-center w-[250px] h-[220px] bg-white rounded-lg flex-col jus justify-center gap-3">
-      <div className="w-[80px] h-[95.1px] flex items-center justify-center relative ">
+      <div className="w-[80px] h-[95.1px] flex items-center justify-center relative">
         <Image
           src={cartProduct.product.photo}
           alt={cartProduct.product.name}
@@ -18,21 +23,33 @@ const CartItem = ({ cartProduct }: ICartItemProps) => {
         />
       </div>
       <div className="flex items-center justify-center w-[211px] h-[21.45px]">
-        <p className="text-[16px] leading-[19px] font-normal">
+        <p className="text-[16px] leading-[19px] font-normal text-black">
           {cartProduct.product.name}
         </p>
       </div>
-      <div className="flex w-[211px] h-[21.45px] justify-between ">
+      <div className="flex w-[211px] h-[21.45px] justify-between">
         <div className=" flex w-[97px] h-[34px]  justify-between border  rounded-[2px] font-[22px] items-center">
-          <span className="border-r-[1px] w-[32.4px] text-center">-</span>
-          <span className="border-r-[1px] w-[32.4px] text-center">
+          <span
+            className="border-r-[1px] text-black w-[32.4px] text-center cursor-pointer"
+            onClick={() => decreaseProductQuantity(cartProduct.product.id)}
+          >
+            -
+          </span>
+          <span className=" text-black border-r-[1px] w-[32.4px] text-center cursor-pointer">
             {cartProduct.quantity}
           </span>
-          <span className="w-[32.4px] text-center">+</span>
+          <span
+            className=" text-black w-[32.4px] text-center cursor-pointer"
+            onClick={() => increaseProductQuantity(cartProduct.product.id)}
+          >
+            +
+          </span>
         </div>
 
         <Button className="w-[84px] h-[34px]">
-          {formatCurrency(Number(cartProduct.product.price))}
+          {formatCurrency(
+            Number(cartProduct.product.price * cartProduct.quantity)
+          )}
         </Button>
       </div>
     </div>
--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -2,15 +2,11 @@ import { useContext } from "react";
 import { CartContext } from "../_context/data-context";
 import CartItem from "./cart-item";
 
-interface CartProps {
-  setIsOpen: (isOpen: boolean) => void;
-}
-
-const Cart = ({ setIsOpen }: CartProps) => {
+const Cart = () => {
   const { cartProducts } = useContext(CartContext);
 
   return (
-    <div className="w-full flex items-center flex-col gap-4">
+    <div className=" overflow-y-scroll h-full pb-[230px] pt-[40px]  flex items-center flex-col gap-4 [&::-webkit-scrollbar]:hidden">
       {cartProducts.map((product) => (
         <span key={product.product.id}>
           <CartItem cartProduct={product} />
