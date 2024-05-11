0349495e6a0f27254351fcad4540bc05e6cc861c
Edson <biduzao.bidu21@gmail.com>
2024-05-11 16:43:18 -0300

    feat: add animation

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -3,8 +3,30 @@ import { useEffect, useState } from "react";
 import ProductItem from "./product-item";
 import { IProducts } from "../_interfaces/interfaces";
 import { api } from "../_api/api";
+import { motion } from "framer-motion";
 
 const ProductList = () => {
+  const parent = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        //you can use these if u want stagger effect on initial stage instead of whileInView
+        delayChildren: 1,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const child = {
+    hidden: { opacity: 0 },
+    pop: (custom: number) => ({
+      opacity: 1,
+      transition: {
+        delay: 0.2 * custom,
+      },
+    }),
+  };
   const [products, setProducts] = useState<IProducts>();
 
   useEffect(() => {
@@ -26,10 +48,18 @@ const ProductList = () => {
   }
   return (
     <div className="flex flex-col gap-4 pb-16 pt-4  md:grid sm:grid-cols-2 lg:grid-cols-4  lg:p-[120px] lg:pt-0 lg:pb-0 ">
-      {products.products.map((product) => (
-        <div className=" flex justify-center items-center" key={product.id}>
+      {products.products.map((product, index) => (
+        <motion.div
+          variants={parent}
+          key={product.id}
+          initial="hidden"
+          animate={{ opacity: 1, y: 0 }}
+          whileInView={"pop"}
+          transition={{ delay: index * 0.05 }} // Ajuste o atraso de transição para cada item
+          className=" flex justify-center items-center"
+        >
           <ProductItem product={product} />
-        </div>
+        </motion.div>
       ))}
     </div>
   );
